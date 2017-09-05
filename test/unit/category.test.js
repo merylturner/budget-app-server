60c33c987f2ea0f528086c2d0d2d8fa7afa4af19
Meryl Turner <merylturner08@gmail.com>
2017-09-04 19:06:03 -0700

    passing unit tests for category

---
+++ test/unit/category.test.js
@@ -0,0 +1,30 @@
+const assert = require('chai').assert;
+const Category = require('../../lib/models/category');
+
+const expectedValidation = () => {
+    throw new Error('expected validation errors');
+};
+
+describe('category model', () => {
+
+    it('validates the model', () => {
+        const category = new Category({
+            name: 'Home',
+            budget: 500,
+            date: '09/04/2017'
+        });
+        return category.validate();
+    });
+
+    it('fails validation with missing fields', () => {
+        const category = new Category();
+
+        return category.validate()
+            .then(expectedValidation, err => {
+                const errors = err.errors;
+                assert.ok(errors.name && errors.name.kind === 'required');
+                assert.ok(errors.budget && errors.budget.kind === 'required');
+                assert.ok(errors.date && errors.date.kind === 'required');
+            });
+    });
+});
